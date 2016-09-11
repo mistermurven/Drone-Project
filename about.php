86730e1ec3857699e62bfbf849df08ad5e2f62e2
mistermurven <mistermurven@yahoo.co.uk>
2016-09-11 22:47:15 +0100

    Setting Up Drone Project Website
    Learning the basics of GitHub, PHPStorm and MAMP and the differences
    between different text editors. Basic progress made with the website.
    Need to work on it more and use more components for the website. Make a
    plan.

---
+++ about.php
@@ -0,0 +1,11 @@
+<?php require_once("assets/header.php"); ?>
+
+    <div class="container">
+        <div class="row">
+            <div class="col-md-12">
+                <h1><i class="btl bt-flag"></i>About Page</h1>
+            </div>
+        </div>
+    </div>
+
+<?php require_once("assets/footer.php"); ?>
